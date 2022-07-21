32639820c33caffe1e74d5d9717226ddbbb48d49
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-07-20 21:42:18 -0700

    trying the destroy all

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+  def trash_em
+    @trash_em = List.where(completed: true).destroy_all
+    redirect_to lists_url, notice: "All items were successfully deleted."
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :lists
+
   root 'lists#index'
 
-  resources :lists
+
+  get 'trash_em', to: 'home#trash_em'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
