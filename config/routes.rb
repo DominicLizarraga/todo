d5123081c21bde47e25b09f930a3553d607b47a7
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-07-21 22:21:27 -0700

    destroy all now working 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   root 'lists#index'
 
-
   get 'trash_em', to: 'home#trash_em'
+  get 'trash_em_all', to: 'home#trash_em_all'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
